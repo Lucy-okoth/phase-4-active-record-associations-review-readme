59590d3c9a0f79aa37929704fe39ab462e5521d8
Lucy okoth <lucyakokoth@gmail.com>
2022-12-01 23:40:40 +0300

    Completed lab

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,6 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
-
 ActiveRecord::Schema.define(version: 2021_05_06_151732) do
 
   create_table "authors", force: :cascade do |t|
